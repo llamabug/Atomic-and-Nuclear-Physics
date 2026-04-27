fb9b43ba851dcd319f75de9da297b6bb0a85f9a9
llamabug <matildajaprea@gmail.com>
2026-04-27 20:42:05 +1200

    hour 4- update script.js

--- script.js
+++ script.js
@@ -18,49 +18,37 @@ document.addEventListener("DOMContentLoaded", function() {
 
 const questions = [
     {
-        question: "What is the charge on an alpha particle?",
-        answers: [
-            {text: "+2", correct: true},
-            {text: "+1", correct: false},
-            {text: "-1", correct: false},
-            {text: "-2", correct: false},
-        ]
+        question: "What is an alpha particle?",
+        answers: ["helium nucleus", "he nucleus",]
     },
     {
-        question: "What is the charge on an beta particle?",
-        answers: [
-            {text: "+2", correct: false},
-            {text: "+1", correct: false},
-            {text: "-1", correct: true},
-            {text: "-2", correct: false},
-        ]
-    },
-    {
-        question: "Which particle is most ionising?",
-        answers: [
-            {text: "alpha", correct: true},
-            {text: "beta", correct: false},
-            {text: "gamma", correct: false},
-            {text: "neutrons", correct: false},
-        ]
-    },
-    {
-        question: "Which particle is most penetrating?",
-        answers: [
-            {text: "alpha", correct: false},
-            {text: "beta", correct: false},
-            {text: "gamma", correct: true},
-            {text: "neutrons", correct: false},
-        ]
+        question: "What is an beta particle?",
+        answers: ["electron","e",]
     },
 ];
 
 const questionElement = document.getElementById("question");
-const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
+const enterButton = document.getElementById("enter-btn")
 
 let currentQuestionIndex = 0;
 let score = 0;
+
+function search() {
+    const input = document.getElementById("search").value.toLowerCase().trim();
+    const currentQuestion = questions[currentQuestionIndex];
+
+    if (currentQuestion.answers.includes(input)) {
+        score++
+        questionElement.innerHTML = "Correct!";
+    } else {
+        questionElement.innerHTML = "Incorrect!";
+    }
+
+    nextButton.style.display = "block";
+    enterButton.style.display = "none";
+}
+
 
 function startQuiz() {
     currentQuestionIndex = 0;
@@ -72,48 +60,21 @@ function startQuiz() {
 function showQuestion() {
     resetState();
     let currentQuestion = questions[currentQuestionIndex];
-    let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+    questionElement.innerHTML = currentQuestion.question;
 
-    currentQuestion.answers.forEach(answer => {
-        const button = document.createElement("button");
-        button.innerHTML = answer.text;
-        button.classList.add("btn");
-        answerButtons.appendChild(button);
-        if(answer.correct) {
-            button.dataset.correct = answer.correct;
-        }
-        button.addEventListener("click", selectAnswer);
-    });
+    document.getElementById("search").value = "";
 }
 
 function resetState() {
     nextButton.style.display = "none";
-    while(answerButtons.firstChild) {
-        answerButtons.removeChild(answerButtons.firstChild);
-    }
+    enterButton.style.display = "block";
 }
 
-function selectAnswer(e) {
-    const selectedBtn = e.target;
-    const isCorrect = selectedBtn.dataset.correct === "true";
-    if(isCorrect) {
-        selectedBtn.classList.add("correct");
-        score++;
-    } else {
-        selectedBtn.classList.add("incorrect");
-    }
-    Array.from(answerButtons.children).forEach(button => {
-        if(button.dataset.correct === "true"){
-            button.classList.add("correct");
-        }
-        button.disabled = true;
-    });
-    nextButton.style.display = "block";
-}
+
 
 function showScore() {
     resetState();
+    enterButton.style.display = "none";
     questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
     nextButton.innerHTML = "Play Again";
     nextButton.style.display = "block";
