c21893ec6263f8b4cf2d149e8e623f38da03e41f
llamabug <matildajaprea@gmail.com>
2026-05-08 18:47:23 +1200

    final update script.js

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-//Accordian
 document.addEventListener("DOMContentLoaded", function () {
-  const acc = document.getElementsByClassName("accordion");
-
-  for (let i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-      this.classList.toggle("active");
-      const panel = this.nextElementSibling;
-      panel.style.display = panel.style.display === "block" ? "none" : "block";
-    });
-  }
-
   startQuiz(); 
 });
-
 
 
 const questions = [
