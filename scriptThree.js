3c65ee68ee31ad1346507da598fb74507439be2d
llamabug <matildajaprea@gmail.com>
2026-05-08 20:11:07 +1200

    final.final update scriptThree.js

--- scriptThree.js
+++ scriptThree.js
@@ -229,3 +229,13 @@ nextButton.addEventListener("click", () => {
 });
 
 enterButton.addEventListener("click", checkInputAnswer);
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    if (enterButton.style.display !== "none") {
+      checkInputAnswer();
+    } else if (nextButton.style.display !== "none") {
+      handleNextButton();
+    }
+  }
+});
